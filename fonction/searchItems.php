f6e31c0c7ae28a3a42b695c32191c5c9c339ab07
SUN Julie <sun.julie0@gmail.com>
2021-12-23 09:22:21 +0100

    Update searchItems.php

--- fonction/searchItems.php
+++ fonction/searchItems.php
@@ -2,5 +2,18 @@
 session_start();
 require_once '../base/dao.php';
 
-$dbConnect = mysqli_connect()
+$dbConnect = mysqli_connect($dsn,$userName,$password);
+$sql = "SELECT * FROM collection WHERE nom_collection LIKE '%".$_POST['name']."%'";
+$result = mysqli_query($dbConnect, $sql);
+if (mysqli_num_rows($result)>0) {
+    while($row=mysqli_fetch_assoc($result)) {
+         echo " <tr>;
+                    <td>".$row['nom_collection']."</td>;
+               </tr>";
+
+    } 
+} else {
+    echo "<tr><td> Aucun resultat trouve</td></tr>";
+}
+
 ?>
