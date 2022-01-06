bbfb9fe2315c3b09c1e33fb4127421106881fb8a
SUN Julie <sun.julie0@gmail.com>
2022-01-06 17:54:05 +0100

    Update dao.php

--- base/dao.php
+++ base/dao.php
@@ -30,7 +30,7 @@ function getUserByLogin($log)
 
     $refPdo = connexion();
 
-    $sql = 'SELECT * FROM users WHERE pseudo =:identifiant';
+    $sql = 'SELECT * FROM users WHERE email_user =:identifiant';
     $stat_user = $refPdo->prepare($sql);
     $stat_user->bindParam(':identifiant', $log, PDO::PARAM_STR);
     $stat_user->execute();
@@ -46,9 +46,10 @@ function setNewUser($user)
 {
     $refPdo = connexion();
 
-    $sql = 'INSERT INTO users VALUES (null, :nom_user, :prenom_user, :adresse_user, :email_user, :psw_user, :tel_user, :categorie_user, :type_membre, :pseudo, :creation_compte);';
+    $sql = 'INSERT INTO users VALUES ( :id_user, :nom_user, :prenom_user, :adresse_user, :email_user, :psw_user, :tel_user, :categorie_user, :creation_date);';
     $stat_user = $refPdo->prepare($sql);
 
+    $stat_user->bindParam(':id_user', $user['id_user'], PDO::PARAM_STR);
     $stat_user->bindParam(':nom_user', $user['nom_user'], PDO::PARAM_STR);
     $stat_user->bindParam(':prenom_user', $user['prenom_user'], PDO::PARAM_STR);
     $stat_user->bindParam(':adresse_user', $user['adresse_user'], PDO::PARAM_STR);
@@ -57,9 +58,8 @@ function setNewUser($user)
     $stat_user->bindParam(':psw_user', $psw, PDO::PARAM_STR);
     $stat_user->bindParam(':tel_user', $user['tel_user'], PDO::PARAM_STR);
     $stat_user->bindParam(':categorie_user', $user['categorie_user'], PDO::PARAM_STR);
-    $stat_user->bindParam(':type_membre', $user['type_membre'], PDO::PARAM_STR);
-    $stat_user->bindParam(':pseudo', $user['pseudo'], PDO::PARAM_STR);
-    $stat_user->bindParam(':creation_compte', $user['creation_compte'], PDO::PARAM_STR);
+    // $stat_user->bindParam(':type_membre', $user['type_membre'], PDO::PARAM_STR);
+    $stat_user->bindParam(':creation_date', $user['creation_date'], PDO::PARAM_STR);
     try {
         $stat_user->execute();
     } catch (PDOException $pdoErr) {
